4e80c2bd74ac230e19c6a03bbcfab6be7d1abe0f
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-08-21 14:45:34 +0400

    adding scss styles and header and footer structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <div className='header__container'>
+          <span>BestShop</span>
+          <nav>
+            <ul>
+              <li>WHY US</li>
+              <li>BENEFITS</li>
+              <li>PRICES</li>
+              <li>CONTACT</li>
+            </ul>
+          </nav>
+        </div>
       </header>
+      <main>
+        main
+      </main>
+      <footer>
+        <div className='footer__container'>
+          <div>
+            <span className='logo logo--footer'>BestShop</span>
+            <p>© 2019 BestShop LTD, All Rights Reserved</p>
+          </div>
+          <div>
+            <a href="http://twitter.com"><img src="../src/assets/Twitter.svg" alt="Twitter Icon"/></a>
+            <a href="http://facebook.com"><img src="../src/assets/Facebook.svg" alt="Facebook Icon"/></a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
