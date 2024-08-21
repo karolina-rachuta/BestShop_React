7780e2c3b3516b9c9e253361a23dbd65ca5d6e21
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-08-21 15:28:41 +0400

    footer styles applied

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ function App() {
       <footer>
         <div className='footer__container'>
           <div>
-            <span className='logo logo--footer'>BestShop</span>
-            <p>© 2019 BestShop LTD, All Rights Reserved</p>
+            <span className='logo--footer'>BestShop</span>
+            <p className='footer__rights'>© 2019 BestShop LTD, All Rights Reserved</p>
           </div>
-          <div>
-            <a href="http://twitter.com"><img src="../src/assets/Twitter.svg" alt="Twitter Icon"/></a>
-            <a href="http://facebook.com"><img src="../src/assets/Facebook.svg" alt="Facebook Icon"/></a>
+          <div className='footer__socials'>
+            <a className="twitter" href="http://twitter.com"></a>
+            <a className="facebook" href="http://facebook.com"></a>
           </div>
         </div>
       </footer>
