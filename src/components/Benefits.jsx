51745de48c1ef80b085ae88076a365a7bf10c876
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-08-23 16:13:58 +0400

    finished styles for first section of benefits, working on styles for second section'

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -6,30 +6,58 @@ function Benefits() {
   return (
     <div className="benefits__container">
       <div className="benefit__container">
-        <div className="benefit__column benefit__column--left">
-            <h3>Be always first</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        <div className=" benefit__column box__text box__text--laptop">
+          <h3>Be always first</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
         </div>
         <div className="benefit__column">
-          <div className="benefit__column--laptop"><img src={Laptop_Picture} alt="Macbook picture" className="img__macbook"/></div>
+          <div className="benefit__column--laptop">
+            <img
+              src={Laptop_Picture}
+              alt="Macbook picture"
+              className="img__macbook"
+            />
+          </div>
         </div>
       </div>
       <div className="benefit__container">
-        <div className="benefit__column benefit__column--left">
-            <h3>Your shop is where you are!</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        <div className="benefit__column box__text box__text--phone">
+          <h3>Your shop is where you are!</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
         </div>
-        <div className="benefit__column benefit__column--phone">
-          <img src={Phone_Picture} alt="Phone picture" className="img__phone"/>
+        <div className="benefit__column">
+          <div className="benefit__column--phone">
+            <img
+              src={Phone_Picture}
+              alt="Phone picture"
+              className="img__phone"
+            />
+          </div>
         </div>
       </div>
       <div className="benefit__container">
-        <div className="benefit__column benefit__column--left">
-            <h3>Increase recognition your brand!</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+        <div className="benefit__column box__text box__text--trumpet">
+          <h3>Increase recognition your brand!</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
         </div>
         <div className="benefit__column benefit__column--right">
-          <img src={Trumpet_Picture} alt="Trumpet picture"className="img__trumpet"/>
+          <img
+            src={Trumpet_Picture}
+            alt="Trumpet picture"
+            className="img__trumpet"
+          />
         </div>
       </div>
     </div>
