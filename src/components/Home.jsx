6029930d5e528ff58416d6dd50d7e2270a501b1c
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-01-08 16:46:51 +0400

    code refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,11 @@ import React, { Suspense } from 'react';
 import Header from './Header';
 import Hero from './Hero';
 const LazyWhyUs = React.lazy(() => import('./WhyUs'));
-// import WhyUs from './WhyUs';
 const LazyBenefits = React.lazy(() => import('./Benefits'));
 const LazyPrices = React.lazy(() => import('./Prices'));
 const LazyCalculator = React.lazy(() => import('./Calculator'));
 const LazyContact = React.lazy(() => import('./Contact'));
 const LazyFooter = React.lazy(() => import('./Footer'));
-// import Benefits from './Benefits';
-// import Prices from './Prices';
-// import Calculator from './Calculator';
-// import Contact from './Contact';
-// import Footer from './Footer';
 
 function Home() {
     return (
