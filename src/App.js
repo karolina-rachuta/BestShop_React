efb05d0baca68659fe9b64c73ec80dc72b8b010e
Karolina Rachuta <karolina.rachuta@gmail.com>
2025-01-08 15:49:19 +0400

    hero preload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Bg_desktop } from '../src/assets/Background@2x.webp';
+import { Bg_desktop } from '../src/assets/bgc-mobile.webp';
+import Home from './components/Home';
 import './App.scss';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    useEffect(() => {
+        //preloading image
+        imageList = [Bg_desktop, Bg_desktop];
+        imageList.forEach((image) => {
+            new Image().src = image;
+        });
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />{' '}
+            </Routes>{' '}
+        </BrowserRouter>
+    );
 }
 
 export default App;
